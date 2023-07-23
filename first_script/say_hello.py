1c92e8349fa00dda412eca4c997c488ae1f4d8c8
Rudychuk <dmytrorudychuk@gmail.com>
2023-07-23 18:50:15 +0300

    Hi!

---
+++ first_script/say_hello.py
@@ -0,0 +1,28 @@
+import socket
+
+def handle_request(request):
+    response = 'HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n'
+    name = request.splitlines()[-1].decode().split('=')[-1]
+    if name:
+        response += f'{name} вітаємо на тестовій сторінці Marketplase'
+    else:
+        response += 'Привіт! Введіть своє ім\'я у параметрах запиту.'
+    return response
+
+def main():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(('localhost', 8080))
+    server_socket.listen(1)
+
+    print('Сервер запущений на http://localhost:8080/')
+
+    while True:
+        client_socket, client_address = server_socket.accept()
+        request_data = client_socket.recv(1024)
+        response_data = handle_request(request_data)
+        client_socket.sendall(response_data.encode())
+        client_socket.close()
+
+if __name__ == '__main__':
+    main()
+
