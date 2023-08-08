4fa3da94ff4b90a347c2b23dc902586b11a70925
RudychukDmytro <dmytrorudychuk@gmail.com>
2023-08-08 21:48:57 +0300

    swaggerTest

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_marshmallow import Marshmallow
+from flask_swagger_ui import get_swaggerui_blueprint
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -23,5 +24,13 @@ def create_app(config_class=Config) -> Flask:
 
     app.register_blueprint(accounts_route)
     app.register_blueprint(test_route)
+
+    
+    SWAGGER_URL = "/swagger"
+    API_URL = "/static/swaggerAuth.json"
+    swaggerui_blueprint = get_swaggerui_blueprint(
+        SWAGGER_URL, API_URL, config={"app_name": "Authentication API"}
+    )
+    app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
 
     return app
