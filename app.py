84b55540577c3dba31cf8d748ce5c717ba4ba2c2
RudychukDmytro <dmytrorudychuk@gmail.com>
2023-09-30 15:10:03 +0300

    cors

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from flask_migrate import Migrate
 from flask_marshmallow import Marshmallow
 from flask_swagger_ui import get_swaggerui_blueprint
 from flask_jwt_extended import JWTManager
-from flask_cors import CORS
 from config import Config
 
 
@@ -14,7 +13,6 @@ db = SQLAlchemy()
 migrate = Migrate()
 ma = Marshmallow()
 jwt = JWTManager()
-cors = CORS()
 
 
 
@@ -26,13 +24,12 @@ def create_app(config_class=Config) -> Flask:
     db.init_app(app)
     migrate.init_app(app, db)
     ma.init_app(app)
-    cors.init_app(app)
 
     jwt.init_app(app)
 
-    CORS(app, origins=["http://localhost:3000", "http://localhost", "http://127.0.0.1", 
-                        "http://0.0.0.0", "https://*ondigitalocean.app", 
-                        "http://*ondigitalocean.app"])
+    CORS(app, origins=["http://localhost", "http://127.0.0.1", "http://0.0.0.0",
+                       "https://*ondigitalocean.app", "http://*ondigitalocean.app"], 
+                       supports_credentials=True)
 
 
     from routes.accounts_route import accounts_route
